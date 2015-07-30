9b65e9b081a70e83b61bea07469a409d3ff9f5e6
pwnerdreamz <pwnerdreamz@gmail.com>
2015-07-30 16:58:36 +0300

    Removed android code and moved it to a new repository

--- android/Tankcontrollingapp/app/src/main/java/control/tank/com/pwner/tankcontrollingapp/JoystickClickedListener.java
+++
@@ -1,6 +0,0 @@
-package control.tank.com.pwner.tankcontrollingapp;
-
-public interface JoystickClickedListener {
-    public void OnClicked();
-    public void OnReleased();
-}
--- android/Tankcontrollingapp/app/src/main/java/control/tank/com/pwner/tankcontrollingapp/JoystickMovedListener.java
+++
@@ -1,9 +0,0 @@
-package control.tank.com.pwner.tankcontrollingapp;
-
-import java.io.IOException;
-
-public interface JoystickMovedListener {
-    public void OnMoved(int pan, int tilt) throws IOException;
-    public void OnReleased();
-    public void OnReturnedToCenter();
-}
